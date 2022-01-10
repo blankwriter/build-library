71c9b9e75b77cdb6be1b1567b74ada48071e5c92
blankwriter <prince.eworho@amalitech.org>
2022-01-10 02:53:53 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+class Media {
+  constructor(title) {
+  this._title = title;
+  this._isCheckedOut = false;
+  this._ratings = [];
+  }
+
+  get title () {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(newIsCheckedOut) {
+    this._isCheckedOut = newIsCheckedOut;
+  }
+
+ toggleCheckOutStatus() {
+  if(this._isCheckedOut == true){
+    return this._isCheckedOut = false;
+  }
+  else {
+    return this._isCheckedOut = true
+  }
+ }
+
+ getAverageRating(){
+   let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+   const lengthOfArray = this._ratings.length;
+
+  return ratingsSum/lengthOfArray;
+  }
+
+
+ addRating(rating) {
+  this._ratings.push(rating);
+ }
+
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+  super(title);
+  this._author = author;
+  this._pages = pages;
+  }
+  get author (){
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+class Movie extends Media {
+  constructor(director, title, runtime) {
+    super(title);
+    this._director = director;
+    this._runtime = runtime;
+  }
+  get director() {
+   return this._director;
+  }
+  get runtime() {
+     return this._runtime;
+  }
+}
+
+const historyOfEverything = new Book ('Bill Bryson', 'A Short History of Nearly Everything', 544);
+ 
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4, 5, 5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie ('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1, 1, 5);
+console.log(speed.getAverageRating());
+
+
+
